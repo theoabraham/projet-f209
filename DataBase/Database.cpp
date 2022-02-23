c40e24d0eea68e2a989bb24e4f2257fdefb65aff
alex <vinovrski.alexandre@ulb.be>
2022-02-23 11:23:40 +0100

    Ajout du dossier DataBase Doit etre convertie en Class

--- DataBase/Database.cpp
+++ DataBase/Database.cpp
@@ -1,36 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <array>
-#include "account.hpp"
+//
+// Created by alex on 2/23/22.
+//
 
-class Database {
-
-    std::string input_file;
-    std::string psw;
-public:
-	Database();
-    void signUp();   //Créer un nouveau compte dans la base de données 
-    bool login();   //Cherche si le compte existe dans la base de données, renvoie True si le compte existe, False sinon
-    void prompt(std::string option) { //Demande à l'utilisateur s'il souhaite s'inscrire ou se connecter 
-        if (option == "signup") {
-            signUp();
-        }
-        login();
-    }
-
-    void saveGame(Account userAccount, std::string saveName);    //Sauvegarde une partie dans le fichier du compte de l'hôte de la partie
-    void loadGame(Account userAccount, std::string saveName);    //Vérifie s'il existe une sauvegarde du nom saveName dans le fichier lié au userAccount
-    void deleteSave(Account userAccount, std::string saveName); //Détruit une sauvegarde du nom saveName dans le fichier lié au userAccount
-
-    void deleteAccount(Account userAccount);    //Efface le fichier lié au userAccount
-    	                                        //renvoie vers la demande prompt()
-                                                
-    void addFriend(Account userAccount, Account friendAccount);  //userAccount demande friendAccount en ami
-    void acceptFriend(Account userAccount,Account friendAccount, bool isAccepted); //Accepte ou refuse la demande en ami de friendAccount envoyé à userAccount
-    void deleteFriend(Account userAccount,Account friendAccount);    //Efface friendAccount de la liste d'amis de userAccount
-};
-/*
+#include "Database.h"
 #include <iostream>
 #include <fstream>
 #include <string>
---
+++ DataBase/Database.h
@@ -0,0 +1,14 @@
+//
+// Created by alex on 2/23/22.
+//
+
+#ifndef PROJET_F209_DATABASE_H
+#define PROJET_F209_DATABASE_H
+
+
+class Database {
+
+};
+
+
+#endif //PROJET_F209_DATABASE_H
