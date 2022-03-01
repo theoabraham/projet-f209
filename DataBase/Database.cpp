ea8062dacf8a0929488da78d81aa42e1f6bb45fd
valexand-ulb <vinovrski.alexandre@ulb.be>
2022-03-01 12:01:41 +0100

    Ajout d'amis fonctionnel

--- DataBase/Database.cpp
+++ DataBase/Database.cpp
@@ -1,7 +1,3 @@
-//
-// Created by alex on 2/23/22.
-//
-
 #include "Database.h"
 
 int DatabaseHandler::parse(const std::string& file_path, std::array<std::string, 6> * arr_addr){
@@ -29,7 +25,7 @@ int DatabaseHandler::parse(const std::string& file_path, std::array<std::string,
     return 1;
 }
 
-int DatabaseHandler::check_pswd(const std::string& input_psw, const std::string& stocked_hash){
+int DatabaseHandler::checkPswd(const std::string& input_psw, const std::string& stocked_hash){
     std::hash<std::string> h;
     if (!(std::to_string(h(input_psw)) == stocked_hash)){ // si hash(psw) != hash_stocké
         return 0;
@@ -38,7 +34,7 @@ int DatabaseHandler::check_pswd(const std::string& input_psw, const std::string&
     return 1;
 }
 
-std::string DatabaseHandler::create_psw(){
+std::string DatabaseHandler::createPsw() {
     std::hash<std::string> hashed;
     std::string psw;std::string pswv;
     do{
@@ -58,19 +54,17 @@ int DatabaseHandler::is_string_valid(const std::string &filepath){
 }
 
 bool DatabaseHandler::does_file_exist(const std::string &filename) {
-    std::ifstream ifile;
-    ifile.open(filename);
-    if (ifile){
+    std::ifstream ifile("Data/"+filename);
+    if (ifile.is_open()){
         return 1;
     }else{
         return 0;
     }
 }
 
-std::string DatabaseHandler::create_file(const std::string& filename){
-    //std::fstream output_file;
-    const std::string board="/////\n"; const std::string win="0\n"; const std::string loosed="0\n";const std::string none="none\n";
-    std::string psw = create_psw() + '\n' ;
+std::string DatabaseHandler::createFile(const std::string& filename){
+    const std::string board="/////\n"; const std::string win="0\n"; const std::string loosed="0\n";const std::string none="none";
+    std::string psw = createPsw() + '\n' ;
     FILE *o_file = fopen(("Data/"+filename).c_str(), "w");
     if (o_file){
         fwrite(psw.c_str(), 1, psw.size(), o_file);
@@ -84,7 +78,7 @@ std::string DatabaseHandler::create_file(const std::string& filename){
     return filename;
 }
 
-void DatabaseHandler::write_friends(const std::string &filename, const std::string &friends_str) {
+void DatabaseHandler::writeFriends(const std::string &filename, const std::string &friends_str) {
     const std::string board="/////\n"; const std::string win=string_arr[2]+"\n";
     const std::string loosed=string_arr[3]+"\n";const std::string none="none\n";
     std::string psw = string_arr[0]+"\n";
@@ -100,7 +94,7 @@ void DatabaseHandler::write_friends(const std::string &filename, const std::stri
     fclose(o_file);
 }
 
-void DatabaseHandler::transfer_friend(){
+void DatabaseHandler::transferFriend() {
     for (std::string s : toAddList){
         std::string ans;
         if (s=="none"){
@@ -121,8 +115,24 @@ void DatabaseHandler::addfriend(const std::string &friendname) {
         std::cout << "Amis innexistant / impossible à ajouter" << std::endl;
         return;
     }
+    writeFriendstoAdd(friendname);
+}
 
-
+void DatabaseHandler::writeFriendstoAdd(const std::string &friends_name) {
+    const std::string board=string_arr[1]+"\n"; const std::string win=string_arr[2]+"\n";
+    const std::string loosed=string_arr[3]+"\n";const std::string none="none";
+    const std::string friends= username+"\n";
+    std::string psw = string_arr[0]+"\n";
+    FILE *o_file = fopen(("Data/"+friends_name).c_str(), "w");
+    if (o_file){
+        fwrite(psw.c_str(), 1, psw.size(), o_file);
+        fwrite(board.c_str(), 1, board.size(), o_file);
+        fwrite(win.c_str(), 1, win.size(), o_file);
+        fwrite(loosed.c_str(), 1, loosed.size(), o_file);
+        fwrite(friends.c_str(), 1, friends.size(), o_file);
+        fwrite(none.c_str(), 1, none.size(), o_file);
+    }
+    fclose(o_file);
 }
 
 DatabaseHandler::DatabaseHandler() {
@@ -149,7 +159,7 @@ DatabaseHandler::DatabaseHandler() {
         if (ans =="n"){
             return;
         }
-        std::string new_file = create_file(input_file);
+        std::string new_file = createFile(input_file);
         parse(new_file, &string_arr);
         return;
     }
@@ -162,20 +172,27 @@ DatabaseHandler::DatabaseHandler() {
     std::cin >> psw;
 
     //vérification du mdp
-    if(!check_pswd(psw, string_arr[0])){
+    if(!checkPswd(psw, string_arr[0])){
         std::cout << "Erreur, mauvais mot de passe." << std::endl;
         return;
     }
     //mise à jours des amis
     std::cout << "______________________________" << std::endl;
     std::cout << "Mise à jours des amis:" << std::endl;
-    transfer_friend();
+    transferFriend();
     //réécriture dans le fichier
     std::string friends_str;
     for (std::string s:friendList){
         friends_str += (s + " ");
     }
-    write_friends(username, friends_str);
+    writeFriends(username, friends_str);
+    std::cout << "______________________________" << std::endl;
+    //ajout d'amis
+    std::cout << "Ajouts d'amis:" << std::endl;
+    std::string friends;
+    std::cout << "Entrez un amis à ajouter: ";
+    std::cin >> friends;
+    addfriend(friends);
     std::cout << "______________________________" << std::endl;
     std::cout << "Construction de la matrice" << std::endl;
     std::cout << "..." << std::endl;
--- DataBase/Database.h
+++ DataBase/Database.h
@@ -33,18 +33,17 @@ public:
     std::vector<std::string>  getFriendList() const {return friendList;}
 
     // Méthode "bool"
-    int check_pswd(const std::string& input_psw, const std::string& stocked_hash);
+    int checkPswd(const std::string& input_psw, const std::string& stocked_hash);
     int is_string_valid(const std::string &filepath);
     bool does_file_exist(const std::string &filename);
     // Méthodes
     int parse(const std::string& file_path, std::array<std::string, 6> * arr_addr);
-    std::string create_psw();
-    std::string create_file(const std::string& filename);
-    void write_friends(const std::string& filename,const std::string &friends_str);
-    void transfer_friend();
+    std::string createPsw();
+    std::string createFile(const std::string& filename);
+    void writeFriends(const std::string& filename,const std::string &friends_str);
+    void writeFriendstoAdd(const std::string &friends_name);
+    void transferFriend();
     void addfriend(const std::string& friendname);
 
 };
-
-
 #endif //PROJET_F209_DATABASE_H
